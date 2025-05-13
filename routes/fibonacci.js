d6fa96163b61764a2cc818279ed95b0ad7f8953f
Omiyata Takato <s2312114@st.tohtech.ac.jp>
2025-05-14 01:13:36 +0900

    Fix: prioritize query route over path param

--- routes/fibonacci.js
+++ routes/fibonacci.js
@@ -2,8 +2,11 @@
 const express = require('express');// サーバー読み込み
 const router = express.Router();//小分けにする
 
-const {GetFibonaci} =require('../controllers/fibonacci_create');//関数もらう
+const {GetFibonaci_Pasupara} =require('../controllers/fibonacci_create');//関数もらう//パスパラメーター
+const {GetFibonaci_Kueri} =require('../controllers/fibonacci_create');//関数もらう//パスパラメーター
 
-router.get('/:n',GetFibonaci);//:nのnを関数に渡す
+router.get('/',GetFibonaci_Kueri);//クエリ式
+
+router.get('/:n',GetFibonaci_Pasupara);//:nのnを関数に渡す　//パスパラメーター式
 
 module.exports = router;//外部で使うためコピー
