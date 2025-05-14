113d5bcbf8b4842bdec9e10562c51944a5862f0e
omiyatakato <s2312114@st.tohtech.ac.jp>
2025-05-14 10:14:21 +0900

    Update fibonacci_create.js

--- controllers/fibonacci_create.js
+++ controllers/fibonacci_create.js
@@ -9,7 +9,7 @@ exports.GetFibonaci_Kueri = (req, res, next) => {
 			return res.status(400).json({status: 400,message:'Bad request'});//エラー内容表示
 		}
 		const result = calcFibonacci(n); //正常な値が入力されたら，関数呼び出し解を得る
-		res.json({return_value:result.toString()});//JSONに返す.書き込む（？）処理
+		res.json({result:result.toString()});//JSONに返す.書き込む（？）処理
 	}
 	catch(err){//catchは処理を止めずに最後に処理してくれる
 		next(err);//エラーポイントに渡す
