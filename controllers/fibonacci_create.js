3ab258a3c0bd502e01943d99e3f5e7e454097b49
omiyatakato <s2312114@st.tohtech.ac.jp>
2025-05-14 09:20:23 +0900

    Update fibonacci_create.js

--- controllers/fibonacci_create.js
+++ controllers/fibonacci_create.js
@@ -6,7 +6,7 @@ exports.GetFibonaci_Kueri = (req, res, next) => {
 		const n = parseInt(req.query.n, 10);//params文字列を整数に変換
 								//↑これはユーザーが送ったデータをサーバに渡しすもの．データにアクセスできる queryは/fib?n=10に対応
 		if (!Number.isInteger(n) || n < 0) {//エラーの判別（文字じゃない,0未満，上限いる？わからぬ）
-			return res.status(400).json({status,message:'Bad request'});//エラー内容表示
+			return res.status(400).json(status(400),{message:'Bad request'});//エラー内容表示
 		}
 		const result = calcFibonacci(n); //正常な値が入力されたら，関数呼び出し解を得る
 		res.json({result});//JSONに返す.書き込む（？）処理
